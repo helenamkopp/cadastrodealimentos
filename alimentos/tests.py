b346506885d3b343f073030c93ddc7f0f2a03843
Helena Kopp <helena.kopp@olist.com>
2021-06-20 11:44:30 -0300

    test_template_create / alimentos

--- alimentos/tests.py
+++ alimentos/tests.py
@@ -18,16 +18,35 @@ class TestAlimentos(TestCase):
         response = self.client.get('/create_food/')
         self.assertEqual(response.status_code, 200)
 
+
     def test_route_read_all(self):
         response = self.client.get('/food/')
         self.assertEqual(response.status_code, 200)
+
 
     def test_route_update(self):
         result = Food.objects.last()
         response = self.client.get(f'/update_food/{result.id}')
         self.assertEqual(response.status_code, 200)
 
-    # def test_route_delete(self):
+
+    def test_route_delete(self):
+        result = Food.objects.last()
+        response = self.client.post(f'/delete_food/{result.id}')
+        self.assertEqual(response.status_code, 302)
+
+    #Views tests
+
+    def test_template_create(self):
+        response = self.client.get('/create_food/')
+        self.assertTemplateUsed(response, 'form_food.html')
+    
+    # def test_template_read_all(self):
     #     pass
 
+    # def test_template_update(self):
+    #     pass
+
+    # def test_template_delete(self):
+    #     pass
 
