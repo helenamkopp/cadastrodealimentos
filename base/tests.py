791accbe7befd780ca68ad08db5cfa8bce8d2753
Helena Kopp <helena.kopp@olist.com>
2021-06-20 11:20:54 -0300

    created tests to template/base and route/base

--- base/tests.py
+++ base/tests.py
@@ -1,3 +1,18 @@
+from django.http import response
 from django.test import TestCase
+from django.urls import reverse
+import pytest
 
-# Create your tests here.
+HOME = 'home'
+
+
+class TestBase(TestCase):
+    
+
+    def test_route_base_home_return_status_code_200(self):
+        response = self.client.get(reverse(HOME))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_route_use_template(self):
+        response = self.client.get(reverse(HOME))
+        self.assertTemplateUsed(response, 'home.html')
