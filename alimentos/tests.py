f923ed5f65b52f2ff3450f05795b5ce2979f73b9
Helena Kopp <helena.kopp@olist.com>
2021-06-20 11:32:23 -0300

    test of route_create/alimentos

--- alimentos/tests.py
+++ alimentos/tests.py
@@ -1,3 +1,29 @@
+from django.http import response
 from django.test import TestCase
+from django.urls import reverse
+from alimentos.models import Food
+import pytest
 
-# Create your tests here.
+
+class TestAlimentos(TestCase):
+
+    def setUp(self):
+        food = Food.objects.create(name='Banana', brand='Seu Ze', section='Frutas', content='6', price='2.95' )
+        food.save()
+        self.response_get = self.client.get('food/')
+
+    # Routes tests
+    def test_route_create(self):
+        response = self.client.get('/create_food/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_route_read(self):
+        pass
+
+    def test_route_update(self):
+        pass
+
+    def test_route_delete(self):
+        pass
+
+
